8d4e1c9f4eeb7acf3bdcf648e0b2b21fc1c7267d
Lê Quốc Việt <vietlqse180751@fpt.edu.vn>
2026-04-22 17:39:33 +0700

    feature address

---
+++ Mirai/Mirai.Infastructure/Repositories/AddressRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mirai.Application.DTO;
+using Mirai.Application.Interfaces.Repositories;
+using Mirai.Domain.Entities;
+using Mirai.Infastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mirai.Infastructure.Repositories
+{
+    public class AddressRepository : GenericRepository<Address>, IAddressRepository
+    {
+        public AddressRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Address?> CreateAddressAsync(AddressDto addressDto)
+        {
+            var address = new Address()
+            {
+                AddressId = Guid.NewGuid().ToString(),
+                UserId = addressDto.UserId,
+                RecipientName = addressDto.RecipientName,
+                RecipientPhone = addressDto.RecipientPhone,
+                AddressLine = addressDto.AddressLine,
+                Ward = addressDto.Ward,
+                District = addressDto.District,
+                City = addressDto.City,
+                Province = addressDto.Province,
+                Note = addressDto.Note,
+                IsDefault = false,
+                CreatedAt = DateTime.Now
+            };
+            await _context.AddAsync(address);
+            await _context.SaveChangesAsync();
+            return address;
+        }
+
+        public async Task<Address?> GetAddressByIdAsync(string addressId)
+        {
+            return await _context.Addresses.Where(a => a.AddressId == addressId).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Address>> GetAddressByUserIdAsync(string userId)
+        {
+            return await _context.Addresses.Where(a => a.UserId == userId).ToListAsync();
+        }
+
+        public async Task<List<Address>?> GetAllAddressesAsync()
+        {
+            return await _context.Addresses.ToListAsync();
+        }
+
+        public async Task<Address?> UpdateAddressAsync(UpdateAddressDto updateAddressDto, string addressId)
+        {
+            var address = _context.Addresses.FirstOrDefault(a => a.AddressId == addressId);
+            if (address == null)
+            {
+                return null;
+            }
+            address.AddressId = addressId;
+            address.RecipientName = updateAddressDto.RecipientName;
+            address.RecipientPhone = updateAddressDto.RecipientPhone;
+            address.AddressLine = updateAddressDto.AddressLine;
+            address.Ward = updateAddressDto.Ward;
+            address.District = updateAddressDto.District;
+            address.City = updateAddressDto.City;
+            address.Province = updateAddressDto.Province;
+            address.Note = updateAddressDto.Note;
+            address.UpdatedAt = DateTime.Now;
+
+            await _context.SaveChangesAsync();
+            return address;
+        }
+    }
+}
---
+++ Mirai/Mirai.Infastructure/Services/AddressService.cs
@@ -0,0 +1,44 @@
+﻿using Mirai.Application.DTO;
+using Mirai.Application.Interfaces.Repositories;
+using Mirai.Application.Interfaces.Services;
+using Mirai.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mirai.Infastructure.Services
+{
+    public class AddressService : IAddressService
+    {
+        public readonly IUnitOfWork _unitOfWork;
+        public AddressService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Address?> CreateAddressAsync(AddressDto addressDto)
+        {
+            return await _unitOfWork.AddressRepository.CreateAddressAsync(addressDto);
+        }
+
+        public async Task<Address?> GetAddressByIdAsync(string addressId)
+        {
+            return await _unitOfWork.AddressRepository.GetAddressByIdAsync(addressId);
+        }
+
+        public async Task<List<Address>> GetAddressByUserIdAsync(string userId)
+        {
+            return await _unitOfWork.AddressRepository.GetAddressByUserIdAsync(userId);
+        }
+
+        public async Task<List<Address>?> GetAllAddressesAsync()
+        {
+            return await _unitOfWork.AddressRepository.GetAllAddressesAsync();
+        }
+
+        public async Task<Address?> UpdateAddressAsync(UpdateAddressDto updateAddressDto, string addressId)
+        {
+            return await _unitOfWork.AddressRepository.UpdateAddressAsync(updateAddressDto, addressId);
+        }
+    }
+}
