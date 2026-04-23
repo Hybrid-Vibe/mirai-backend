e2bb2be7202b6db6456bfe89a6db7a8358c3a2da
Lê Quốc Việt <vietlqse180751@fpt.edu.vn>
2026-04-23 09:25:33 +0700

    full feature brands

---
+++ Mirai/Mirai.Application/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mirai.Application.Interfaces.Repositories
+{
+    internal interface ICategoryRepository
+    {
+    }
+}
